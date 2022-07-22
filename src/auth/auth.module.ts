13ddca7920b2dcd43ca41789d93034d1cfc59c91
André Meireles <andre2meireles@gmail.com>
2022-07-22 16:53:36 -0300

    feature: add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,39 @@
+import { User } from '.prisma/client';
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from 'argon2';
 import { DatabaseService } from 'src/database/database.service';
 import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private database: DatabaseService) {}
+  constructor(
+    private database: DatabaseService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async createUser(auth: AuthDto) {
     const hash = await argon.hash(auth.password);
-    const newUser = await this.database.user.create({
-      data: {
-        email: auth.email,
-        hash: hash,
-      },
-    });
+    try {
+      const newUser = await this.database.user.create({
+        data: {
+          email: auth.email,
+          hash: hash,
+        },
+      });
+      return this.createToken(newUser);
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('credentials exists');
+        }
+      }
 
-    return newUser;
+      throw err;
+    }
   }
 
   async login(auth: AuthDto) {
@@ -30,6 +47,18 @@ export class AuthService {
       throw new ForbiddenException('credentials not found');
     }
 
-    return user;
+    return this.createToken(user);
+  }
+
+  private async createToken(user: User) {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
   }
 }
