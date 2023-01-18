1ae4fcce4e47d8673411d315b725ae8c06029b2c
Péricles Pires <periclespires@protonmail.com>
2023-01-18 18:34:53 -0300

    fix(auth): ignoring jwt auth in signIn and signUp endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { SkipJwt } from './decorators/skip-jwt.decorator';
 import SignInDTO from './dto/signin.dto';
 import SignUpDTO from './dto/signup.dto';
 
@@ -8,12 +9,14 @@ import SignUpDTO from './dto/signup.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SkipJwt()
   @UseGuards(AuthGuard('local'))
   @Post('/signin')
   signIn(@Body() credentials: SignInDTO) {
     return this.authService.signIn(credentials);
   }
 
+  @SkipJwt()
   @Post('/signup')
   signUp(@Body() credentials: SignUpDTO) {
     return this.authService.signUp(credentials);
