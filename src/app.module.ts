b98cb39b8ab4ad69261a8affa9f0c03cce599181
Péricles Pires <periclespires@protonmail.com>
2023-01-18 18:19:09 -0300

    feat(database): orm configurations to connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/database';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    UserModule,
   ],
   controllers: [],
   providers: [],
