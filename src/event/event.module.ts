5c88e505442b98a88392c3699a07c503f77ca00a
Péricles Pires <periclespires@protonmail.com>
2023-01-19 10:46:27 -0300

    fix(event): set IsEventOwner guard as provider

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -4,11 +4,12 @@ import EventImage from './entities/event-image.entity';
 import Event from './entities/event.entity';
 import { EventsController } from './event.controller';
 import { EventsService } from './event.service';
+import { IsEventOwner } from './guards/is-event-owner.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event, EventImage])],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, IsEventOwner],
   exports: [],
 })
 export class EventModule {}
