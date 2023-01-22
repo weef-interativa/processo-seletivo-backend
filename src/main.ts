4819da4033d18a7e659bcc3f69e98372b4a1a6bd
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-22 18:08:20 -0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,25 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Event manager API')
+    .setDescription(
+      'This API allows an user to managenment events, creating, updating or deleting.',
+    )
+    .setVersion('1.0')
+    .addTag('events')
+    .addBearerAuth()
+
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
   await app.listen(3005);
 }
 bootstrap();
