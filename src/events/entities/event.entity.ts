2f712950c3a953a269b3cc11826a39fb90b36a74
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-22 18:09:05 -0300

    feat: added swagger into entities

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -11,47 +11,60 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'events' })
 export class Event {
+  @ApiProperty()
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @ApiProperty()
   @Column({ name: 'user_id' })
   userId: number;
 
+  @ApiProperty()
   @Column({ name: 'event_address_id' })
   eventAddressId: number;
 
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty()
   @Column({ name: 'event_date' })
   eventDate: Date;
 
+  @ApiProperty()
   @Column()
   responsible: string;
 
+  @ApiProperty()
   @Column()
   email: string;
 
+  @ApiProperty()
   @Column()
   phone: string;
 
   @OneToMany(() => EventImage, (event_image) => event_image.event)
+  @ApiProperty({ type: () => EventImage, isArray: true })
   images: EventImage[];
 
   @ManyToOne(() => EventAddress, (event_address) => event_address.events)
   @JoinColumn({ name: 'event_address_id', referencedColumnName: 'id' })
+  @ApiProperty({ type: () => EventAddress })
   address: EventAddress;
 
   @ManyToOne(() => User, (user) => user.events)
   @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
   user: User;
 
+  @ApiProperty()
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
--- src/events/entities/event_address.entity.ts
+++ src/events/entities/event_address.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -10,33 +11,44 @@ import { Event } from './event.entity';
 
 @Entity({ name: 'event_address' })
 export class EventAddress {
+  @ApiProperty()
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @OneToMany(() => Event, (event) => event.address)
+  @OneToMany(() => Event, (event) => event.address, {
+    onDelete: 'CASCADE',
+  })
   events: Event[];
 
+  @ApiProperty()
   @Column({ name: 'zip_code' })
   zipCode: string;
 
+  @ApiProperty()
   @Column()
   state: string;
 
+  @ApiProperty()
   @Column()
   city: string;
 
+  @ApiProperty()
   @Column()
   street: string;
 
+  @ApiProperty()
   @Column()
   number: number;
 
+  @ApiProperty()
   @Column({ nullable: true })
   complement: string;
 
+  @ApiProperty()
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
