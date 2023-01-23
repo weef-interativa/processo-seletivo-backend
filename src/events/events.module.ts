91906c92911e2f9d457b67a7398a07862e8c86f5
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-23 01:47:41 -0300

    feat: add events in modules

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,3 +1,4 @@
+import { EventsAddressService } from './services/events-address.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './../users/entities/user.entity';
@@ -5,13 +6,19 @@ import { Event } from './entities/event.entity';
 import { EventAddress } from './entities/event_address.entity';
 import { EventImage } from './entities/event_image.entity';
 import { EventsController } from './events.controller';
-import { EventsService } from './events.service';
 import { EventCloudinaryService } from './services/events-cloudinary-upload.service';
+import { EventsImageService } from './services/events-images.service';
+import { EventsService } from './services/events.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Event, EventImage, EventAddress])],
   controllers: [EventsController],
-  providers: [EventsService, EventCloudinaryService],
+  providers: [
+    EventsService,
+    EventCloudinaryService,
+    EventsImageService,
+    EventsAddressService,
+  ],
   exports: [EventsService],
 })
 export class EventsModule {}
